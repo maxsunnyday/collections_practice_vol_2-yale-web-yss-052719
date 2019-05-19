e582b3261fe7b0a830a42b0793d7957b2def0cbf
Max Sun <max.sun@yale.edu>
2019-05-19 19:54:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
-# your code goes here
+require "pry"
+def begins_with_r(array)
+  array.each do |word|
+    letters = word.split("")
+    if letters[0] != "r"
+      return false
+      break
+    else
+    end
+  end
+  true
+end
+
+def contain_a(array)
+  a_words = []
+  array.each do |word|
+    letters = word.split("")
+    letters.each do |letter|
+      if letter == "a"
+        a_words << word
+        break
+      end
+    end
+  end
+  a_words
+end
+
+def first_wa(array)
+  array.each do |word|
+    string = word.to_s
+    letters = string.split("")
+    if letters[0] == "w" && letters[1] == "a"
+      return word
+      break
+    end
+  end
+end
+
+def remove_non_strings(array)
+  new_array = []
+  array.each do |thing|
+    if thing == thing.to_s
+      new_array << thing
+    end
+  end
+  new_array
+end
+
+def count_elements(array)
+  new_array = []
+
+end
